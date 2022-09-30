ab7c91ae80adf8fdf2f12bc7d5b8d2f9e454233d
Alexander <ajveats@gmail.com>
2022-09-30 15:23:18 +0100

    Completed lab flask work

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+# app = Flask(__name__)
+
+
+# @app.route('/hello/')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+
+
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['name']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('name')
+        return redirect(url_for('success', name=user))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
